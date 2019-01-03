6cd9644863de88d1d032af52a382f7788599204f
Ayupov-Ayaz <ayaz.ayupov@gmail.com>
2019-01-03 23:09:09 +0300

    collbacks

--- pointers.go
+++ pointers.go
@@ -6,51 +6,53 @@ import(
 )
 
 /**
-	Указатели - это переменные которые хранят ссылку в области памяти
+	Указатели - это переменные которые хранят ссылку в область памяти
 */
 func main() {
-	var spaceCount int8 = 16
-	
-	fmt.Printf(getTableTitleFormat(14), "Имя переменной ", "Тип переменной", "Значение", "Адрес")
-	printSpace(spaceCount)	
-	var intVar int8 = 10
-	fmt.Printf(getTableFormat(15), "intVar ", intVar, intVar, &intVar )
-	printSpace(spaceCount)
-
-	firstPointer := &intVar
-	fmt.Printf(getTableFormat(15), "firstPointer", firstPointer, firstPointer, &firstPointer)
-	fmt.Printf(getTableFormat(15), "*firstPointer", *firstPointer, *firstPointer, &*firstPointer)
-	printSpace(spaceCount)
-
-	secondPointer := &firstPointer
-	fmt.Printf(getTableFormat(15), "secondPointer", secondPointer, secondPointer, &secondPointer)
-	fmt.Printf(getTableFormat(15), "*secondPointer", *secondPointer, *secondPointer, &*secondPointer)
-	fmt.Printf(getTableFormat(15), "**secondPointer", **secondPointer, **secondPointer, &**secondPointer)
-	printSpace(spaceCount)
-
-	
-	var thirdPointer *int
-	fmt.Printf(getTableFormat(15), "thirdPointer", thirdPointer, thirdPointer, &thirdPointer)
-	printSpace(spaceCount)
-}
-
-func printSpace(spaceCount int8) {
+	var titleSpaceCount = 14
+	var tableSpaceCount int = 15
 	var space string
-	var i int8 = 0
-	for i < spaceCount{
+	
+	var i int = 0
+	for i < 16 {
 		space += "-"
 		i++ 
 	}
+	
+	printSpace := func() {	
+		fmt.Printf("|%s |%s|%s|%s|\n",space,space,space,space)
+	}
 
-	fmt.Printf("|%s |%s|%s|%s|\n",space,space,space,space)
-}
+	getTableTitleFormat := func() string {
+		count := strconv.Itoa(titleSpaceCount)
+		return "| %-" + count + "s | %-14s | %-14s | %-14s |\n"
+	}
 
-func getTableFormat(spaceCount int) string {
-	count := strconv.Itoa(spaceCount)
-	return "| %-" + count + "v | %-14T | %-14v | %-14p |\n";
-}
+	getTableFormat := func() string {
+		count := strconv.Itoa(tableSpaceCount)
+		return "| %-" + count + "v | %-14T | %-14v | %-14p |\n";
+	}
 
-func getTableTitleFormat(spaceCount int) string {
-	count := strconv.Itoa(spaceCount)
-	return "| %-" + count + "s | %-14s | %-14s | %-14s |\n"
+	fmt.Printf(getTableTitleFormat(), "Имя переменной ", "Тип переменной", "Значение", "Адрес")
+	printSpace()	
+	var intVar int8 = 10
+	fmt.Printf(getTableFormat(), "intVar ", intVar, intVar, &intVar )
+	printSpace()
+
+	firstPointer := &intVar
+	fmt.Printf(getTableFormat(), "firstPointer", firstPointer, firstPointer, &firstPointer)
+	fmt.Printf(getTableFormat(), "*firstPointer", *firstPointer, *firstPointer, &*firstPointer)
+	printSpace()
+
+	secondPointer := &firstPointer
+	fmt.Printf(getTableFormat(), "secondPointer", secondPointer, secondPointer, &secondPointer)
+	fmt.Printf(getTableFormat(), "*secondPointer", *secondPointer, *secondPointer, &*secondPointer)
+	fmt.Printf(getTableFormat(), "**secondPointer", **secondPointer, **secondPointer, &**secondPointer)
+	printSpace()
+
+	
+	var thirdPointer *int
+	fmt.Printf(getTableFormat(), "thirdPointer", thirdPointer, thirdPointer, &thirdPointer)
+	printSpace()
+
 }
