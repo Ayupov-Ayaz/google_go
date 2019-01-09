357b96fba0a2fa911ba2827476d5424de0be30c8
= <=>
2019-01-09 09:50:16 +0300

    работа с константами

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	ent "google_go/Entities"
 	f "google_go/functions"
 	lang "google_go/language"
 	"google_go/types"
-	"sync"
 )
 
 func main() {
@@ -19,11 +17,11 @@ func main() {
 	types.TestStruct()
 	duck := new(ent.Duck)
 	lang.CookingDuck(duck)
-
-	fmt.Println("\nПотоки: У Main всегда главный поток, который не ждет другие.")
-	// Создание WaitGroup всегда по указателю, нет смысла его копировать
-	wg := &sync.WaitGroup{}
-	lang.TestGo2(3, wg)
-	lang.TestChannel()
-	wg.Wait()
+	lang.TestConst()
+	//fmt.Println("\nПотоки: У Main всегда главный поток, который не ждет другие.")
+	//// Создание WaitGroup всегда по указателю, нет смысла его копировать
+	//wg := &sync.WaitGroup{}
+	//lang.TestGo2(3, wg)
+	//lang.TestChannel()
+	//wg.Wait()
 }
