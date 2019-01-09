efbea9d560eb5858beeb4fb2b154c4f99db3c595
ayaz.ayupov <ayaz.ayupov>
2019-01-09 16:48:49 +0300

    интерфейсы углубленный разбор 1

--- language/interfaces.go
+++ language/interfaces.go
@@ -1,14 +1,29 @@
 package language
-import(
+
+import (
 	"fmt"
-	interfaces "google_go/Interfaces"
+	"google_go/Interfaces"
 )
 // в Go нигде не указывается, что тип имплементирует интерфейс
 // Тут идет принцип Утиной типизации
-func CookingDuck(duck interfaces.DuckFamily) {
+func CookingDuck(duck Interfaces.DuckFamily) {
 	fmt.Println("Если наш тип умеет:")
 	duck.Swim()
 	duck.Fly()
 	duck.Quack()
 	fmt.Println("Тогда это утка! Весь принцип интерфесов в языке Go-lang построен на утиной типизации")
+	GoFly(duck)
+}
+
+// Проверка на то, что наш тип интерпритирует интерфейс
+func GoFly(i interface{}) {
+	if flyer, ok := i.(Interfaces.Flyer); ok {
+		flyer.Fly()
+	}
+}
+
+func TestFly(iFlyer Interfaces.Flyer) {
+	//if duck, ok := iFlyer.(Entities.Duck); ok {
+	//	fmt.Println(duck)
+	//}
 }
