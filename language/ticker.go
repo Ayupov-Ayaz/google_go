63032add654b68fda4a1f1f9d4db7e9089d7f3aa
Аюпов Аяз <ayaz.ayupov@gmail.com>
2019-01-12 22:21:51 +0300

    ticker

--- language/ticker.go
+++ language/ticker.go
@@ -18,3 +18,22 @@ func StartTicker(howLongSec int) {
 	}
 	fmt.Println("total ", t)
 }
+
+/**
+	Бесконечный тикер
+	Если завершить цикл и не выйти из программы то тикер будет продолжать работать
+	Его нужно использовать тогда, когда вы точно не собираетесь его останавливать: Мониторинг, сбор и отсылка каких либо
+ 	данных и т.д
+ */
+func InfinityTicker(howLongSec int) {
+	ticker := time.Tick(time.Second)
+	t := 0
+	// сразу возвращает канал с которого можно считать
+	for chanel := range ticker {
+		t++
+		fmt.Printf("step %d, time: %v \n", t, chanel)
+		if t >= howLongSec {
+			break
+		}
+	}
+}
