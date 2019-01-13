d775d2d89d2d86ccacfa36e36e206ea0fe0765f8
Аюпов Аяз <ayaz.ayupov@gmail.com>
2019-01-13 22:18:22 +0300

    clear main.go

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"google_go/Entities"
-	"google_go/functions"
-	lang "google_go/language"
-	"google_go/types"
-)
-
 func main() {
-	lang.TestSwitch()
-	functions.StartDefer()
-	functions.StartInit()
-	functions.StartPanicAndRecover()
-	lang.TestRune()
-	lang.RunFor()
-	types.TestMaps()
-	lang.TestAreaOfVisibility()
-	lang.TestPointers()
-	types.TestStruct()
-	duck := new(Entities.Duck)
-	lang.CookingDuck(duck)
-	lang.TestConst()
-	types.TestSlice()
-	lang.TestChannel()
-	lang.RunGoroutines()
+	//
 }
