12f6b92c2c1191c4a877126f2c83e2ac124e7ae2
Аюпов Аяз <ayaz.ayupov@gmail.com>
2019-01-13 22:52:15 +0300

    serve mux. Запуск нескольких серверов в разных горутинах

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "google_go/web"
+
 func main() {
 	//
+	go web.StartServeMux(8000)
+	web.StartServeMux(8001)
+
+
 }
--- web/serve_mux.go
+++ web/serve_mux.go
@@ -8,21 +8,23 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "1 Handler, URL:", r.URL.String())
+	fmt.Fprintln(w, "URL:", r.URL.String())
 }
-
-func StartServerMux(port int) {
+/**
+	Создание сервера через мультиплексер предоставляет больше возможностей для настройки
+ */
+func StartServeMux(port int) {
 	// Используется для поднятия нескольких разных урлов или несколько разных серверов
 	mux := http.NewServeMux()
 	//можем создать несколько обработчиков, которые обрабатывают одни и те же url-ы но по разному
 	mux.HandleFunc("/", handler)
 	sPort := ":" + strconv.Itoa(port)
 	server := http.Server{
-		Addr: sPort,
-		Handler: mux,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		Addr: sPort, // порт который сервер будет обрабатывать
+		Handler: mux, // мультиплексор обработчик запросов
+		ReadTimeout: 10 * time.Second, // время ожидания на чтение
+		WriteTimeout: 10 * time.Second, // время ожидание на запись
 	}
-	fmt.Printf("Starting server at %s", sPort)
+	fmt.Printf("Starting server at %s \n", sPort)
 	server.ListenAndServe()
 }
